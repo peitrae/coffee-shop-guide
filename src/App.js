723212384f83d70446acba12d91a07b1da3ce13d
Peitra Erdi <peitrae@gmail.com>
2019-12-02 01:09:01 +0700

    Add Footer

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ import ProfilePage from "./views/ProfilePage/ProfilePage";
 import CoffeeShop from "./views/CoffeeShop/CoffeeShop";
 import UpdateCoffeeShop from "./views/UpdateCoffeeShop/UpdateCoffeeShop";
 import * as actions from "./store/actions/member";
+import Footer from "./components/UI/Footer/Footer";
 
-const App = () => {
+const App = props => {
+  const hasPreference = useSelector(state => state.member.preference);
 
-  const hasPreference = useSelector(state => state.member.preference)
-
-  const dispatch = useDispatch()
-  const authCheckState = useCallback(preference => dispatch(actions.authCheckState(preference)), [dispatch])
+  const dispatch = useDispatch();
+  const authCheckState = useCallback(
+    preference => dispatch(actions.authCheckState(preference)),
+    [dispatch]
+  );
 
   useEffect(() => {
     authCheckState(hasPreference);
@@ -24,19 +27,16 @@ const App = () => {
   return (
     <React.Fragment>
       <NavBar />
-      <Switch>
-        <Route path="/" exact component={Homepage} />
-        <Route path="/search" exact component={Search} />
-        <Route path="/coffee-shop/:id" exact component={CoffeeShop} />
-        <Route path="/update-coffee-shop" exact component={UpdateCoffeeShop} />
-        <Route
-          path="/update-coffee-shop/:id"
-          exact
-          component={UpdateCoffeeShop}
-        />
-        <Route path="/profile" exact component={ProfilePage} />
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/search" exact component={Search} />
+          <Route path="/coffee-shop/:id" exact component={CoffeeShop} />
+          <Route path="/update-coffee-shop" exact component={UpdateCoffeeShop} />
+          <Route path="/update-coffee-shop/:id" exact component={UpdateCoffeeShop} />
+          <Route path="/profile" exact component={ProfilePage} />
+          <Redirect to="/" />
+        </Switch>
+      { (props.location.pathname === "/" ) ? null : <Footer />}
     </React.Fragment>
   );
 };
---
+++ src/components/UI/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import classes from './Footer.module.css'
+
+const Footer = () => <div className={classes.Footer}></div>;
+
+export default Footer;
