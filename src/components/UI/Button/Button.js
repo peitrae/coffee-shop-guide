8ecfc84edd9e254c85f614bf9e6fbc49e76db199
Peitra Erdi <peitrae@gmail.com>
2019-12-28 02:11:11 +0700

    Change className Button to Btn

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,17 +2,12 @@ import React from "react";
 
 import classes from "./Button.module.css";
 
-export const BtnMedium = props => {
-  const btnMedium = [classes.ButtonMedium, classes[props.btnType]].join(" ");
-  return (
-    <button className={btnMedium} onClick={props.clicked}>
-      {props.btnName}
-    </button>
-  );
-};
-
 export const BtnLarge = props => {
-  const btnLarge = [classes.ButtonLarge, classes[props.btnType], [props.className]].join(" ");
+  const btnLarge = [
+    classes.BtnLarge,
+    classes[props.btnType],
+    [props.className]
+  ].join(" ");
   return (
     <button className={btnLarge} onClick={props.clicked}>
       {props.btnName}
@@ -20,8 +15,30 @@ export const BtnLarge = props => {
   );
 };
 
+export const BtnMedium = props => {
+  const btnMedium = [classes.BtnMedium, classes[props.btnType]].join(" ");
+  return (
+    <button className={btnMedium} onClick={props.clicked}>
+      {props.children}
+    </button>
+  );
+};
+
+export const BtnMediumText = props => {
+  const style = [classes.BtnMediumText, classes[props.btnType]].join(" ");
+  return (
+    <button className={style} onClick={props.clicked}>
+      {props.children}
+    </button>
+  );
+};
+
 export const BtnSmall = props => {
-  const btnSmall = [classes.ButtonSmall, classes[props.btnType], [props.className]].join(" ");
+  const btnSmall = [
+    classes.BtnSmall,
+    classes[props.btnType],
+    [props.className]
+  ].join(" ");
   return (
     <button className={btnSmall} onClick={props.clicked}>
       {props.children}
