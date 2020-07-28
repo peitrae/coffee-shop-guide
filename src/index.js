addbe720a64099b1c7453dd2f1ecdfeee71918aa
Peitra Erdi <peitrae@gmail.com>
2020-07-28 23:37:34 +0700

    Delete unused dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
 
 import "./index.css";
@@ -29,7 +28,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(watchMember);
