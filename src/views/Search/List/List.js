ee4467342b63731a35becbae9e86e05682f569aa
Peitra Erdi <peitrae@gmail.com>
2020-10-25 11:59:16 +0700

    Delete useState

--- src/views/Search/List/List.js
+++ src/views/Search/List/List.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Filter from "./Filter/Filter";
 import BestRecommendation from "./BestRecommendation/BestRecommendation";
