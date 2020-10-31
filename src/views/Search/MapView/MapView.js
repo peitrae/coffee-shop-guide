2ef690f98d19a1a846a06576045e7f3e05629f98
Peitra Erdi <peitrae@gmail.com>
2020-11-01 02:15:37 +0700

    Add user location

--- src/views/Search/MapView/MapView.js
+++ src/views/Search/MapView/MapView.js
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
 
 import Spinner from "../../../components/UI/Spinner/Spinner";
 
 import "./MapView.scss";
 
 const MapView = ({ coffeeShops }) => {
-  const [position] = useState({
+  const [config, setConfig] = useState({
     lat: -7.983,
     long: 112.621,
     zoom: 13,
   });
+
+  const userMarker = new Icon({
+    iconUrl: "/user-marker.svg",
+    iconSize: [35, 35],
+  });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((location) => {
+        setConfig({
+           ...position,
+           lat: location.coords.latitude,
+           long: location.coords.longitude,
+         });
+      });
+    }
+  }, []);
 
   if (!coffeeShops) {
     return <Spinner />;
@@ -26,14 +44,22 @@ const MapView = ({ coffeeShops }) => {
 
   return (
     <Map
-      center={[position.lat, position.long]}
-      zoom={position.zoom}
+      center={[config.lat, config.long]}
+      zoom={config.zoom}
       className="mapview"
     >
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker
+        onMouseOver={onMouseOverMarker}
+        onMouseOut={onMouseOutMarker}
+        position={[config.lat, config.long]}
+        icon={userMarker}
+      >
+        <Popup>You</Popup>
+      </Marker>
       {coffeeShops.map(({ id, name, location }) => {
         if (!name || !location) {
           return null;
