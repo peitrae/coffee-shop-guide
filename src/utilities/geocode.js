f353db954fb8f9f0a05ad91e2fff8d2d87a74218
Peitra Erdi <peitrae@gmail.com>
2020-11-09 13:35:17 +0700

    Fix env typo

--- src/utilities/geocode.js
+++ src/utilities/geocode.js
@@ -1,13 +1,11 @@
 const geocode = async (address) => {
   const response = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.RECT_APP_MAPS_API}`
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_MAPS_API}`
   );
 
   const json = await response.json();
   const lat = json.results[0].geometry.location.lat;
   const long = json.results[0].geometry.location.lng;
-
-  console.log(lat, long);
 
   return { lat, long };
 };
