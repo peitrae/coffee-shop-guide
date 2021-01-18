96237cdf2f2035d373146aba26264e15f4d3fa6f
Peitra Erdi <peitrae@gmail.com>
2021-01-18 17:50:25 +0700

    Disable useEffect eslint

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -22,6 +22,7 @@ export default function useFetch(url, { method, body }) {
     };
 
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { response, error };
