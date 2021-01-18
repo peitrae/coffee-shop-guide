e90ecfd94fefa1b2d02fa447c41c718c23e37df5
Peitra Erdi <peitrae@gmail.com>
2021-01-18 17:52:56 +0700

    Change profile matching  with  new parameter

--- src/utilities/profileMatching.js
+++ src/utilities/profileMatching.js
@@ -1,61 +1,92 @@
-const profileMatching = (preference, coffeeShops) => {
+import { getDistance } from "geolib";
+
+const profileMatching = (preference, userLocation, coffeeShops) => {
   if (!preference || !coffeeShops) {
     return null;
   }
 
-  const noCandidateData = [];
-  const candidateData = [];
-  const coreFactorIndex = [];
-  const secondaryFactorIndex = [];
-  const coreFactor = [];
-  const secondaryFactor = [];
-
-  const checkHasRating = (coffeeShops) => {
-    coffeeShops.forEach((coffeeShop) => {
-      coffeeShop.feedback
-        ? candidateData.push(coffeeShop)
-        : noCandidateData.push(coffeeShop);
-    });
+  const getAverage = (arr) => {
+    return (
+      arr.reduce((accumulator, currentValue) => accumulator + currentValue) /
+      arr.length
+    );
   };
 
-  checkHasRating(coffeeShops);
-
-  preference.forEach((value, index) => {
-    value >= 4 ? coreFactorIndex.push(index) : secondaryFactorIndex.push(index);
-  });
-
-  const avgCoffeeShopRating = (feedback) => {
-    const rating = [];
-    for (let key in feedback) {
-      rating.push(feedback[key].rating);
+  const getCandidateAspects = (coffeeShops) => {
+    const getPriceRange = (averagePrice) => {
+      if (averagePrice < 10000) return 1;
+      else if (averagePrice >= 10000 && averagePrice < 30000) return 2;
+      else if (averagePrice >= 30000 && averagePrice <= 50000) return 3;
+      else return 4;
     };
 
-    return rating.reduce((prev, curr) => [
-      (prev[0] + curr[0]) / rating.length,
-      (prev[1] + curr[1]) / rating.length,
-      (prev[2] + curr[2]) / rating.length,
-    ]);
+    const getDistanceGroup = (distance) => {
+      if (distance > 10000) {
+        return 5;
+      } else if (distance > 5000) {
+        return 4;
+      } else if (distance > 3000) {
+        return 3;
+      } else if (distance > 1000) {
+        return 2;
+      } else if (distance > 0) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+
+    const getTastesAndCoffeePresentations = (feedback) => {
+      const tastes = [];
+      const coffeePresentations = [];
+
+      Object.keys(feedback).forEach((key) => {
+        tastes.push(feedback[key].rating[0]);
+        coffeePresentations.push(feedback[key].rating[1]);
+      });
+
+      return { tastes, coffeePresentations };
+    };
+
+    const candidateData = coffeeShops.map((coffeeShop) => {
+      const distance = getDistance(
+        { latitude: userLocation.lat, longitude: userLocation.long },
+        {
+          latitude: coffeeShop.location.lat,
+          longitude: coffeeShop.location.long,
+        }
+      );
+
+      const { tastes, coffeePresentations } = getTastesAndCoffeePresentations(
+        coffeeShop.feedback
+      );
+
+      return {
+        id: coffeeShop.id,
+        distance: getDistanceGroup(distance),
+        averagePrice: getPriceRange(coffeeShop.averagePrice),
+        taste: getAverage(tastes),
+        coffeePresentations: getAverage(coffeePresentations),
+        data: { ...coffeeShop },
+      };
+    });
+
+    return candidateData;
   };
 
-  const searchPriceRange = (averagePrice) => {
-    if (averagePrice < 10000) return 1;
-    else if (averagePrice >= 10000 && averagePrice < 30000) return 2;
-    else if (averagePrice >= 30000 && averagePrice <= 50000) return 3;
-    else return 4;
+  const getGap = (candidates) => {
+    return candidates.map((candidate) => ({
+      id: candidate.id,
+      distance: candidate.distance - expectedValue[0],
+      averagePrice: candidate.averagePrice - expectedValue[1],
+      taste: Math.floor(candidate.taste - expectedValue[2]),
+
+      data: candidate.data,
+    }));
   };
 
-  const candidateValue = candidateData.map((candidate) => {
-    const priceRange = searchPriceRange(candidate.averagePrice);
-    const avgRating = avgCoffeeShopRating(candidate.feedback);
-    return [...avgRating, priceRange];
-  });
-
-  const gap = candidateValue.map((arr) =>
-    arr.map((num, index) => num - preference[index])
-  );
-
-  const weighting = gap.map((arr) => {
-    let temp = arr.map((value) => {
+  const getWeighting = (gap) => {
+    const getWeightingItem = (value) => {
       switch (value) {
         case 0:
           return 5;
@@ -78,66 +109,64 @@ const profileMatching = (preference, coffeeShops) => {
         default:
           return 0;
       }
-    });
-    return temp;
-  });
+    };
 
-  const average = (arr) => {
-    return (
-      arr.reduce((accumulator, currentValue) => accumulator + currentValue) /
-      arr.length
-    );
+    return gap.map((candidate) => ({
+      id: candidate.id,
+      distance: getWeightingItem(candidate.distance),
+      averagePrice: getWeightingItem(candidate.averagePrice),
+      taste: getWeightingItem(candidate.taste),
+      data: candidate.data,
+    }));
   };
 
-  weighting.forEach((arr) => {
-    let coreFactorTemp = [];
-    let secondaryFactorTemp = [];
-    arr.forEach((num, index) => {
-      coreFactorIndex.includes(index)
-        ? coreFactorTemp.push(num)
-        : secondaryFactorTemp.push(num);
+  const getProfileMatching = (weighting) => {
+    return weighting.map((candidate) => {
+      const coreFactor = getAverage([
+        candidate.distance,
+        candidate.averagePrice,
+      ]);
+      const secondaryFactor = getAverage([candidate.taste]);
+
+      const profileMatching = coreFactor * 0.6 + secondaryFactor * 0.4;
+
+      return {
+        ...candidate.data,
+        id: candidate.id,
+        profileMatching,
+      };
     });
-    if (coreFactorTemp.length) coreFactor.push(average(coreFactorTemp));
-    if (secondaryFactorTemp.length)
-      secondaryFactor.push(average(secondaryFactorTemp));
-  });
+  };
 
-  const total = [];
-  for (let index in candidateData) {
-    let result = 0;
-    if (coreFactor.length && secondaryFactor.length) {
-      result = coreFactor[index] * 0.6 + secondaryFactor[index] * 0.4;
-    } else if (coreFactor.length) {
-      result = coreFactor[index];
-    } else if (secondaryFactor.length) {
-      result = secondaryFactor[index];
+  const expectedValue = [...preference, 5, 5];
+  const candidates = [];
+  const notCandidates = [];
+
+  coffeeShops.forEach((coffeeShop) => {
+    if (coffeeShop.feedback) {
+      candidates.push(coffeeShop);
+    } else {
+      notCandidates.push(coffeeShop);
     }
-    total.push(result);
-  }
-
-  const injectIndex = total.map((num, index) => [index, num]);
-  const sortByValue = injectIndex.sort((a, b) => a[1] - b[1]).reverse();
-  const sortedCandidateData = sortByValue.map((value) => {
-    return { ...candidateData[value[0]], profileMatching: value[1].toFixed(1) };
   });
-  const resultProfileMatching = [...sortedCandidateData, ...noCandidateData];
+
+  const candidateAspects = getCandidateAspects(candidates);
+  const gap = getGap(candidateAspects);
+  const weighting = getWeighting(gap);
+  const result = getProfileMatching(weighting);
+  const sortedResult = result
+    .sort((a, b) => a.profileMatching - b.profileMatching)
+    .reverse();
 
   // DEBUG
-  // console.log("preference", preference);
-  // console.log("candidateData", candidateData);
-  // console.log("candidateValue", candidateValue);
-  // console.log("coreFactorIndex", coreFactorIndex);
-  // console.log("secondaryFactorIndex", secondaryFactorIndex);
+  // console.log("expectedValue", expectedValue);
+  // console.log("candidates", candidates);
   // console.log("gap", gap);
   // console.log("weighting", weighting);
-  // console.log("coreFactor", coreFactor);
-  // console.log("secondaryFactor", secondaryFactor);
-  // console.log("total", total);
-  // console.log("injectIndex", injectIndex);
-  // console.log("sortByValue", sortByValue);
-  // console.log("sortedCandidateData", sortedCandidateData);
+  // console.log("result", result);
+  // console.log("sortedResult", sortedResult);
 
-  return resultProfileMatching;
+  return [...sortedResult, ...notCandidates];
 };
 
 export default profileMatching;
