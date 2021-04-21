bece4f1bd9ebd4c88330cc957ae2aa4ac42bf4ae
Peitra Erdi <peitrae@gmail.com>
2021-04-22 02:04:32 +0700

    Add new 404 error type

--- src/components/ErrorMessage/index.js
+++ src/components/ErrorMessage/index.js
@@ -1,36 +1,38 @@
-import React from "react";
+import React from 'react';
 
 const ErrorMessage = ({ children, className }) => {
-  const checkErrorMessage = (message) => {
-    switch (message) {
-      case "INVALID_EMAIL":
-        return "Invalid Email";
-      case "WEAK_PASSWORD : Password should be at least 6 characters":
-        return "Password should be at least 6 characters";
-      case "MISSING_EMAIL":
-        return "Email is empty";
-      case "INVALID_PASSWORD":
-        return "Invalid Password";
-      case "MISSING_PASSWORD":
-        return "Password is empty";
-      case "EMAIL_NOT_FOUND":
-        return "Email not found";
-      case "EMAIL_EXISTS":
-        return "The email address is already in use by another account";
-      case "PASSWORD_NOT_MATCH":
-        return "Password not match";
-      case "CREDENTIAL_TOO_OLD_LOGIN_AGAIN":
-        return "Please login again";
-      default:
-        return message;
-    }
-  };
+	const checkErrorMessage = (message) => {
+		switch (message) {
+			case 'INVALID_EMAIL':
+				return 'Invalid Email';
+			case 'WEAK_PASSWORD : Password should be at least 6 characters':
+				return 'Password should be at least 6 characters';
+			case 'MISSING_EMAIL':
+				return 'Email is empty';
+			case 'INVALID_PASSWORD':
+				return 'Invalid Password';
+			case 'MISSING_PASSWORD':
+				return 'Password is empty';
+			case 'EMAIL_NOT_FOUND':
+				return 'Email not found';
+			case 'EMAIL_EXISTS':
+				return 'The email address is already in use by another account';
+			case 'PASSWORD_NOT_MATCH':
+				return 'Password not match';
+			case 'CREDENTIAL_TOO_OLD_LOGIN_AGAIN':
+				return 'Please login again';
+			case 'Request failed with status code 404':
+				return 'Terjadi gangguan koneksi. Mohon coba lagi';
+			default:
+				return message;
+		}
+	};
 
-  return (
-    <div className={`error-message ${className}`}>
-      {checkErrorMessage(children)}
-    </div>
-  );
+	return (
+		<div className={`error-message ${className}`}>
+			{checkErrorMessage(children)}
+		</div>
+	);
 };
 
 export default ErrorMessage;
