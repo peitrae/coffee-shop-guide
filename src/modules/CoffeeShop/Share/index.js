bcf812f940115b7c2379a106788173151e319443
Peitra Erdi <peitrae@gmail.com>
2021-04-22 16:29:54 +0700

    Move share to modules and delete share by email and whatsapp

---
+++ src/modules/CoffeeShop/Share/index.js
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from 'react';
+import { useLocation } from 'react-router';
+
+import ShareButton from './ShareButton';
+import ShareBubbleBox from './ShareBubbleBox';
+
+const Share = () => {
+	const shareButtonRef = useRef();
+	const location = useLocation();
+
+	const [showShare, setShowShare] = useState(false);
+
+	const handleClickOutside = (e) => {
+		if (shareButtonRef.current.contains(e.target)) {
+			return;
+		}
+
+		setShowShare(!showShare);
+	};
+
+	const handleCopyLink = (text) => {
+		navigator.clipboard.writeText(text);
+	};
+
+	return (
+		<div className="main__share">
+			<ShareButton
+				onClick={() => setShowShare(!showShare)}
+				ref={shareButtonRef}
+			/>
+			{showShare ? (
+				<ShareBubbleBox
+					link={process.env.REACT_APP_URL + location.pathname}
+					handleClose={handleClickOutside}
+					handleCopyLink={handleCopyLink}
+				/>
+			) : null}
+		</div>
+	);
+};
+
+export default Share;
--- src/modules/CoffeeShop/index.js
+++ src/modules/CoffeeShop/index.js
@@ -1,1 +1,2 @@
 export { default as AddFeedback } from './AddFeedback';
+export { default as Share } from './Share';
